4748d1ddb1734088de1ab8417831818845fdc180
Adam Zinder and Luke Philips <adam@>
2016-11-02 15:45:55 -0700

    play tic tac toe but still replaces on clicks

--- js/scripts.js
+++ js/scripts.js
@@ -5,9 +5,16 @@ this.board = [[0,0,0],[0,0,0],[0,0,0]];
 
 
 UIboard = new Board();
- Board.prototype.assignCoordinates = function (x,y) {
-   this.board [x][y] = 1
+Board.prototype.assignCoordinates = function (x,y,player) {
+
+  if (player === "X") {
+    this.board [x][y] = 1
   }
+  else {
+    this.board [x][y] = 4
+  }
+}
+
 Board.prototype.rowWin = function () {
   var rowZeroTotal= 0;
   for (var i = 0 ; i < 3; i++) {
@@ -23,7 +30,10 @@ Board.prototype.rowWin = function () {
   }
   var rowTotal =[rowZeroTotal, rowOneTotal, rowTwoTotal];
   if (rowTotal.includes(3)) {
-    alert("you won!")
+    alert("X win!")
+    }
+  else if (rowTotal.includes(12)) {
+    alert("O wins!")
     }
   return rowTotal;
 }
@@ -42,14 +52,39 @@ Board.prototype.colWin = function () {
   }
   var colTotal =[colZeroTotal, colOneTotal, colTwoTotal];
   if (colTotal.includes(3)) {
-    alert("you won!")
+    alert("X wins!")
     }
+    else if (colTotal.includes(12)) {
+      alert("O wins!")
+      }
   return colTotal;
 }
 Board.prototype.diaWin = function () {
   if (UIboard.board[1][1] === 1 && ((UIboard.board[2][2] === 1 && UIboard.board[0][0] === 1) || (UIboard.board[0][2] === 1 && UIboard.board[2][0] === 1))) {
-    alert("you won!")
+    alert("X wins!")
   }
+  else if (UIboard.board[1][1] === 4 && ((UIboard.board[2][2] === 4 && UIboard.board[0][0] === 4) || (UIboard.board[0][2] === 4 && UIboard.board[2][0] === 4))) {
+    alert("O wins!")
+  }
+}
+
+var countTotal = 9;
+var player = "";
+
+var counter = function(){
+  countTotal -= 1;
+  userPlayer(countTotal);
+};
+
+var userPlayer = function(countTotal) {
+  if (countTotal % 2 === 0) {
+    player = "X";
+  }
+  else {
+    player = "O";
+  }
+console.log(player);
+return player
 }
 
 var coordinates = function (cell) {
@@ -58,18 +93,19 @@ var coordinates = function (cell) {
   var inputy = inputx.splice(0,1)
   inputx = parseInt(inputx);
   inputy = parseInt(inputy);
-  UIboard.assignCoordinates(inputx, inputy)
+  UIboard.assignCoordinates(inputx, inputy, player)
 }
 
 // user interface logic
 $(document).ready(function() {
   $('.cell').click(function(){
-
+    counter();
     coordinates(this);
-    console.log(UIboard);
-    $(this).text("X");
+    debugger;
+    $(this).text(player);
      UIboard.diaWin();
      UIboard.rowWin();
      UIboard.colWin();
+     console.log(UIboard);
   });
 });
