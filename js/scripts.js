58550156ddf4448e2a673445eb2d7cc8123fa923
Adam Zinder and Luke Philips <luke@>
2016-11-02 14:42:41 -0700

    fully functional single player tic tac toe

--- js/scripts.js
+++ js/scripts.js
@@ -8,14 +8,48 @@ UIboard = new Board();
  Board.prototype.assignCoordinates = function (x,y) {
    this.board [x][y] = 1
   }
-Board.prototype.rowWin = function (x){
-  var total  = 0;
-  for (var i = 0; i < 3; i++) {
-    if (this.board[0][i] === 1) {
-       total += 1;
-     }
+Board.prototype.rowWin = function () {
+  var rowZeroTotal= 0;
+  for (var i = 0 ; i < 3; i++) {
+    rowZeroTotal += UIboard.board [0][i];
   }
-  return total;
+  var rowOneTotal= 0;
+  for (var i = 0 ; i < 3; i++) {
+    rowOneTotal += UIboard.board [1][i];
+  }
+  var rowTwoTotal= 0;
+  for (var i = 0 ; i < 3; i++) {
+    rowTwoTotal += UIboard.board [2][i];
+  }
+  var rowTotal =[rowZeroTotal, rowOneTotal, rowTwoTotal];
+  if (rowTotal.includes(3)) {
+    alert("you won!")
+    }
+  return rowTotal;
+}
+Board.prototype.colWin = function () {
+  var colZeroTotal= 0;
+  for (var i = 0 ; i < 3; i++) {
+    colZeroTotal += UIboard.board [i][0];
+  }
+  var colOneTotal= 0;
+  for (var i = 0 ; i < 3; i++) {
+    colOneTotal += UIboard.board [i][1];
+  }
+  var colTwoTotal= 0;
+  for (var i = 0 ; i < 3; i++) {
+    colTwoTotal += UIboard.board [i][2];
+  }
+  var colTotal =[colZeroTotal, colOneTotal, colTwoTotal];
+  if (colTotal.includes(3)) {
+    alert("you won!")
+    }
+  return colTotal;
+}
+Board.prototype.diaWin = function () {
+  if (UIboard.board[1][1] === 1 && ((UIboard.board[2][2] === 1 && UIboard.board[0][0] === 1) || (UIboard.board[0][2] === 1 && UIboard.board[2][0] === 1))) {
+    alert("you won!")
+  }
 }
 
 var coordinates = function (cell) {
@@ -34,6 +68,8 @@ $(document).ready(function() {
     coordinates(this);
     console.log(UIboard);
     $(this).text("X");
-    //alert(UIboard.rowWin());
+     UIboard.diaWin();
+     UIboard.rowWin();
+     UIboard.colWin();
   });
 });
