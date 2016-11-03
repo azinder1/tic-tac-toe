bcc4038549422402078e147d48ecf966cdbc75d2
Adam Zinder and Luke Philips <luke@>
2016-11-03 11:56:27 -0700

    adds styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,30 @@
 //Business logic
-function Board () {
+function Player() {     // two properties of our object Player
+  // this.turn = player;
+  this.playerX = "X";
+  this.playerO = "O";
+}
+var userPlayer = new Player();
+
+Player.prototype.assignTurn = function(countTotal) { //Assigns object property based on turn counter
+  player = "";
+  if (countTotal % 2 === 0) {
+    player = userPlayer.playerX;
+  }
+  else {
+    player = userPlayer.playerO;
+  }
+return player;
+}
+
+function Board () { //object with array property that creates a empty grid
 this.board = [[0,0,0],[0,0,0],[0,0,0]];
 };
 
 UIboard = new Board();
-Board.prototype.assignCoordinates = function (x,y,player) {
-  if (player === "X") {
+
+Board.prototype.assignArrayValues = function (x,y,player) { //takes in coordinates of the click and assigns unique values based on player value
+  if (player === userPlayer.playerX) {
     this.board [x][y] = 1
   }
   else {
@@ -13,7 +32,7 @@ Board.prototype.assignCoordinates = function (x,y,player) {
   }
 }
 
-Board.prototype.rowWin = function () {
+Board.prototype.rowWin = function () { //sums row array values and determines winner based on total equaling 3 (X) or 12 (O)
   var winnerRow = "";
   var rowTotal =[0, 0, 0];
   for (j=0;j<3;j++) {
@@ -30,7 +49,7 @@ Board.prototype.rowWin = function () {
   return winnerRow;
 }
 
-Board.prototype.colWin = function () {
+Board.prototype.colWin = function () { //sums column array values and determines winner based on total equaling 3 (X) or 12 (O)
   var winnerCol = "";
   var colTotal =[0, 0, 0];
   for (j=0;j<3;j++) {
@@ -38,7 +57,6 @@ Board.prototype.colWin = function () {
       colTotal[j] += UIboard.board [i][j];
     }
   }
-
   if (colTotal.includes(3)) {
     winnerCol = winnerCol.concat("X Wins!");
   }
@@ -48,8 +66,7 @@ Board.prototype.colWin = function () {
   return winnerCol;
 }
 
-
-Board.prototype.diaWin = function () {
+Board.prototype.diaWin = function () { //scans array based on diagnal values to see if diagnals of 1 (X) or 4 (O) won
   if (UIboard.board[1][1] === 1 && ((UIboard.board[2][2] === 1 && UIboard.board[0][0] === 1) || (UIboard.board[0][2] === 1 && UIboard.board[2][0] === 1))) {
     return ("X Wins!");
   }
@@ -59,33 +76,24 @@ Board.prototype.diaWin = function () {
 }
 
 var countTotal = 9;
-var player = "";
 
-var counter = function(){
+var counter = function(){ //decraments based on click count on unclicked cells
   countTotal -= 1;
-  userPlayer(countTotal);
+  userPlayer.assignTurn(countTotal)
 };
 
-var userPlayer = function(countTotal) {
-  if (countTotal % 2 === 0) {
-    player = "X";
-  }
-  else {
-    player = "O";
-  }
-return player
-}
 
-var coordinates = function (cell) {
+
+var coordinates = function (cell) { //takes in class names and removes superfluous names to provide x and y coordinate
   var inputx = cell.className;
   inputx = inputx.slice(-5).replace(/\D+/g, '').split("");
-  var inputy = inputx.splice(0,1)
+  var inputy = inputx.splice(1,1)
   inputx = parseInt(inputx);
   inputy = parseInt(inputy);
-  UIboard.assignCoordinates(inputx, inputy, player)
+  UIboard.assignArrayValues(inputx, inputy, player)
 }
 
-var champion = function() {
+var champion = function() { //returns string based on what win-type occured
   var diaWin = UIboard.diaWin()
   var colWin = UIboard.colWin()
   var rowWin = UIboard.rowWin()
@@ -106,6 +114,7 @@ $(document).ready(function() {
   $('.cell').one("click", function(){
     counter();
     coordinates(this);
+
     $(this).text(player);
      var whosTheWinner = champion ()
      $('.winner').text(whosTheWinner);
