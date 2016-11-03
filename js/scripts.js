400a31189c3d15956f1a467470426384c9a5b5bd
Adam Zinder and Luke Philips <adam@>
2016-11-03 09:07:46 -0700

    refactored row and column prototypes

--- js/scripts.js
+++ js/scripts.js
@@ -3,10 +3,8 @@ function Board () {
 this.board = [[0,0,0],[0,0,0],[0,0,0]];
 };
 
-
 UIboard = new Board();
 Board.prototype.assignCoordinates = function (x,y,player) {
-
   if (player === "X") {
     this.board [x][y] = 1
   }
@@ -16,21 +14,13 @@ Board.prototype.assignCoordinates = function (x,y,player) {
 }
 
 Board.prototype.rowWin = function () {
-  debugger;
-  var rowZeroTotal= 0;
   var winnerRow = "";
-  for (var i = 0 ; i < 3; i++) {
-    rowZeroTotal += UIboard.board [0][i];
+  var rowTotal =[0, 0, 0];
+  for (j=0;j<3;j++) {
+    for (var i = 0 ; i < 3; i++) {
+      rowTotal[j] += UIboard.board [j][i];
+    }
   }
-  var rowOneTotal= 0;
-  for (var i = 0 ; i < 3; i++) {
-    rowOneTotal += UIboard.board [1][i];
-  }
-  var rowTwoTotal= 0;
-  for (var i = 0 ; i < 3; i++) {
-    rowTwoTotal += UIboard.board [2][i];
-  }
-  var rowTotal =[rowZeroTotal, rowOneTotal, rowTwoTotal];
   if (rowTotal.includes(3)) {
     winnerRow = winnerRow.concat("X Wins!");
   }
@@ -39,22 +29,16 @@ Board.prototype.rowWin = function () {
   }
   return winnerRow;
 }
-Board.prototype.colWin = function () {
 
-  var colZeroTotal= 0;
+Board.prototype.colWin = function () {
   var winnerCol = "";
-  for (var i = 0 ; i < 3; i++) {
-    colZeroTotal += UIboard.board [i][0];
+  var colTotal =[0, 0, 0];
+  for (j=0;j<3;j++) {
+    for (var i = 0 ; i < 3; i++) {
+      colTotal[j] += UIboard.board [i][j];
+    }
   }
-  var colOneTotal= 0;
-  for (var i = 0 ; i < 3; i++) {
-    colOneTotal += UIboard.board [i][1];
-  }
-  var colTwoTotal= 0;
-  for (var i = 0 ; i < 3; i++) {
-    colTwoTotal += UIboard.board [i][2];
-  }
-  var colTotal =[colZeroTotal, colOneTotal, colTwoTotal];
+
   if (colTotal.includes(3)) {
     winnerCol = winnerCol.concat("X Wins!");
   }
@@ -63,6 +47,8 @@ Board.prototype.colWin = function () {
   }
   return winnerCol;
 }
+
+
 Board.prototype.diaWin = function () {
   if (UIboard.board[1][1] === 1 && ((UIboard.board[2][2] === 1 && UIboard.board[0][0] === 1) || (UIboard.board[0][2] === 1 && UIboard.board[2][0] === 1))) {
     return ("X Wins!");
@@ -87,7 +73,6 @@ var userPlayer = function(countTotal) {
   else {
     player = "O";
   }
-console.log(player);
 return player
 }
 
@@ -100,31 +85,30 @@ var coordinates = function (cell) {
   UIboard.assignCoordinates(inputx, inputy, player)
 }
 
-// var champion = function() {
-//   if (UIboard.diaWin()) {
-//     return this;
-//   }
-//   else if (UIboard.colWin()) {
-//     return this;
-//   }
-//   else if (UIboard.rowWin()) {
-//     return this;
-//   }
-// }
+var champion = function() {
+  var diaWin = UIboard.diaWin()
+  var colWin = UIboard.colWin()
+  var rowWin = UIboard.rowWin()
+
+  if (diaWin) {
+    return diaWin;
+  }
+  else if (colWin) {
+    return colWin;
+  }
+  else if (rowWin) {
+    return rowWin;
+  }
+}
 
 // user interface logic
 $(document).ready(function() {
   $('.cell').one("click", function(){
     counter();
     coordinates(this);
-    debugger;
     $(this).text(player);
-    // var whosTheWinner = champion ()
-    // $('.winner').text(whosTheWinner);
-    $('.winner').append( UIboard.diaWin());
-    $('.winner').append(UIboard.colWin());
-    $('.winner').append(UIboard.rowWin());
-     console.log(UIboard);
+     var whosTheWinner = champion ()
+     $('.winner').text(whosTheWinner);
   });
   $("#refresh").click(function() {
     location.reload();
